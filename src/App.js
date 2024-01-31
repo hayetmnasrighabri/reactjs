f2bf448662d9ab9609b8ca0bb1d0ff3160faad7b
hayetmnasri <hayetmnasri@hotmail.fr>
2024-01-31 15:58:21 +0100

    decrementer

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import { useState } from "react";
 
 function App() {
   const [count, setCount] = useState(0)
-
+    
   const Increment = () => {
       setCount (count + 1)
         console.log(setCount)
+}
+const Decrement = ()=>{
+ setCount(count-1)
 }
   return (
     <>
     <p>compteur: {count}</p>
    <button onClick={Increment}>increment</button>
+   <button onClick={Decrement}>decrement</button>
    </>
   )
 }
