1a147d867d5634e8c7aa33b848a6e028b7e9deb2
hayetmnasri <hayetmnasri@hotmail.fr>
2024-01-31 15:39:13 +0100

    incremanter

--- src/App.js
+++ src/App.js
@@ -1,32 +1,19 @@
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0)
+
+  const Increment = () => {
+      setCount (count + 1)
+        console.log(setCount)
+}
   return (
     <>
-    <Title color="green">Mon composants</Title>
-    <input type="text"/> : 
-      <p>React est une bibliothèque
-       JavaScript pour le rendu des 
-       interfaces utilisateur (UI).
-        L'interface utilisateur est construite à partir 
-        de petites unités telles que des boutons, 
-        du texte et des images. React vous permet de les 
-        combiner en composants réutilisables et
-         emboîtables. Des sites Web aux applications téléphoniques,
-          tout ce qui apparaît à l’écran peut être décomposé en composants. Dans ce chapitre, vous apprendrez à créer, personnaliser et afficher de manière 
-      conditionnelle des composants React.</p>
-    </>
-  );
+    <p>compteur: {count}</p>
+   <button onClick={Increment}>increment</button>
+   </>
+  )
 }
 
-function Title({color, children}) {
-  const props=
-  {
-    id:"A",
-    className:"c"
-  }
-  return(
-  <h2 style={{color:color}} {...props}>{children}</h2>
-  ) 
-}
   
 export default App;
