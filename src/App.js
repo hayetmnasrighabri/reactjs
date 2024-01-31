c827697f452e57cba6e34c1125086ebbfae3eb43
hayetmnasri <hayetmnasri@hotmail.fr>
2024-01-31 10:31:20 +0100

    preventDefault et stopPropagation

---
+++ src/App.js
@@ -0,0 +1,27 @@
+const style={color: 'red', backgroundColor: 'blue'}
+
+function App() {
+  const handleClick=(e)=>{
+        e.preventDefault()
+        e.stopPropagation()
+        alert("j'ai click sur le titre")
+      
+  }
+  return (
+    <>
+    <h2 onClick={handleClick} id="title" className="title" style={style}>bonjour</h2>
+    <input type="text"/>
+    <p>React est une bibliothèque
+       JavaScript pour le rendu des 
+       interfaces utilisateur (UI).
+        L'interface utilisateur est construite à partir 
+        de petites unités telles que des boutons, 
+        du texte et des images. React vous permet de les 
+        combiner en composants réutilisables et
+         emboîtables. Des sites Web aux applications téléphoniques,
+          tout ce qui apparaît à l’écran peut être décomposé en composants. Dans ce chapitre, vous apprendrez à créer, personnaliser et afficher de manière 
+      conditionnelle des composants React.</p>
+    </>
+  );
+}
+export default App
