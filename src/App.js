c275809a6757da6808eb625c4760a1ebdd6d2a29
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-05 15:35:19 +0100

    Ajout à un tableau

--- src/App.js
+++ src/App.js
@@ -1,103 +1,30 @@
-import { useState } from 'react';
+import { useState } from "react"
+let nextId=0
+export default function List(){
+ const [name, setName]=useState('')
+  const [artists, setArtists]=useState([])
 
-export default function Form() {
-  const [person, setPerson] = useState({
-    name: 'Niki de saint phallé',
-    artwork:{
-    title: 'blue Nana',
-    city: 'hamburg',
-    image: 'https://i.imgur.com/Sd1AgUOm.jpg'
-    }
-  });
-   const handlenameChange =(e)=>{
-    setPerson({
-      ...person,
-      name: e.target.value
-    })
-   }
-   const handleTitleChange=(e)=>{
-    setPerson({
-      ...person,
-      artwork:{
-        ...person.artwork,
-        title:e.target.value
-      }
-    })
-   }
-   const handleCityChange=(e)=>{
-    setPerson({
-      ...person,
-      artwork:{
-        ...person.artwork,
-        city: e.target.value
-      }
-    })
-   }
-   const handleImageChange=(e)=>{
-      setPerson({
-        ...person,
-        artwork:{
-          ...person.artwork,
-          image:e.target.value
-        }
-      })
-   }
-
-
-  return (
+  return(
     <>
-      <label>
-        name:
-        <input
-          value={person.name}
-          onChange={handlenameChange}
-        />
-      </label>
-       <br/>
-       <br/>
-       <br/>
-      <label>
-        title:
-        <input
-          value={person.artwork.title}
-          onChange={handleTitleChange}
-        />
-      </label>
-      <br/>
-       <br/>
-       <br/>
-      <label>
-        city:
-        <input
-          value={person.artwork.city}
-          onChange={handleCityChange}
-        />
-      </label>
-      <br/>
-       <br/>
-       <br/>
-      <label>
-        image:
-        <input 
-        value={person.artwork.image}
-        onChange={handleImageChange}
-        />
-      </label>
-      <br/>
-       <br/>
-       <br/>
-       <img 
-      src={person.artwork.image}
-      alt={person.artwork.title}
-      />
-      <p>
-        <i>{person.artwork.title}</i>
-        {' by '}
-        {person.name}
-        <br />
-        (located in {person.artwork.city})
-      </p>
-      
+     <h1>Inspiring sculptors:</h1>
+     <input
+          value={name}
+          onChange={e=> setName(e.target.value)}
+     />
+     <button
+        onClick={()=>{
+          setArtists([
+            ...artists,
+            {id:nextId++,
+            name:name
+            }
+          ])
+        }}>Add</button>
+     <ul>
+      {artists.map(artists=> (
+        <li key={artists.id}>{artists.name}</li>
+      ))}
+     </ul>
     </>
   )
 }
