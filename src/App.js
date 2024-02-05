42b57a87cd464ca968affcb4c88e8c5369fa5f0b
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-05 08:46:10 +0100

    l'état en lecture seule

--- src/App.js
+++ src/App.js
@@ -1,16 +1,30 @@
 import { useState } from 'react';
-
-export default function Counter() {
-  const [number, setNumber] = useState(0);
-
+export default function MovingDot() {
+  const [position, setPosition] = useState({
+    x: 0,
+    y: 0
+  });
   return (
-    <>
-      <h1>{number}</h1>
-      <button onClick={() => {
-        setNumber(number+5)
-        setNumber(n=>n + 1)
-        setNumber(42)
-      }}>Increase the number</button>
-    </>
-  )
+    <div
+      onPointerMove={e => {
+        position.x = e.clientX;
+        position.y = e.clientY;
+      }}
+      style={{
+        position: 'relative',
+        width: '100vw',
+        height: '100vh',
+      }}>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'red',
+        borderRadius: '50%',
+        transform: `translate(${position.x}px, ${position.y}px)`,
+        left: -10,
+        top: -10,
+        width: 20,
+        height: 20,
+      }} />
+    </div>
+  );
 }
