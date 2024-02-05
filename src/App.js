138cf71d2102758595b52d6162900fbeac7cfa2b
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-05 18:43:53 +0100

    Remplacer des éléments dans un tableau

--- src/App.js
+++ src/App.js
@@ -1,49 +1,37 @@
-import { useState } from "react"
+import { useState } from 'react';
 
-let initialShapes=[
-  {id: 0,  type:'circle', x:50, y:100},
-  { id: 1, type: 'square', x: 150, y: 100 },
-  { id: 2, type: 'circle', x: 250, y: 100 },
-]
+let initialCounters = [
+  0, 0, 0
+];
 
-export default function ShapeEditor(){
-  const [shapes, setShapes]=useState(initialShapes)
- const handleClick=()=>{
-  const nextShapes=shapes.map(shape=>{
-    if(shape.type==='square') {
-      return shape
-    } else {
-      return {
-        ...shape,
-        y:shape.y+50
+export default function CounterList() {
+  const [counters, setCounters] = useState(
+    initialCounters
+  );
+
+  function handleIncrementClick(index) {
+    const nextCounters = counters.map((c, i) => {
+      if (i === index) {
+        // Increment the clicked counter
+        return c + 1;
+      } else {
+        // The rest haven't changed
+        return c;
       }
-    }
-    }
-  )
-  setShapes(nextShapes)
- }
-  return (
-    <>
-    <button onClick={handleClick}>
-      move circles down!
-    </button>
-    {shapes.map(shape=> (
-      <div key={shape.id}
-      style={{
-        background: 'purple',
-          position: 'absolute',
-          left: shape.x,
-          top: shape.y,
-          borderRadius:
-            shape.type === 'circle'
-              ? '50%' : '',
-          width: 20,
-          height: 20,
-      }}
-      >
+    });
+    setCounters(nextCounters);
+  }
 
-      </div>
-    ))}
-    </>
-  )
+  return (
+    <ul>
+      {counters.map((counter, i) => (
+        <li key={i}>
+          {counter}
+          <button onClick={() => {
+            handleIncrementClick(i);
+          }}>+1</button>
+        </li>
+      ))}
+    </ul>
+  );
 }
