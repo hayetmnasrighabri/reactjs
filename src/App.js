85b84d07172d1a5cc5e7b02240159b289f1e29d7
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-07 15:05:51 +0100

    Éviter les contradictions d'état

--- src/App.js
+++ src/App.js
@@ -1,77 +1,44 @@
-import { useState } from 'react';
-
-let nextId = 3;
-const initialList = [
-  { id: 0, title: 'Big Bellies', seen: false },
-  { id: 1, title: 'Lunar Landscape', seen: false },
-  { id: 2, title: 'Terracotta Army', seen: true },
-];
-
-export default function BucketList() {
-  const [myList, setMyList] = useState(initialList);
-  const [yourList, setYourList] = useState(
-    initialList
-  );
-
-  function handleToggleMyList(artworkId, nextSeen) {
-    setMyList(myList.map(artwork => {
-      if (artwork.id === artworkId) {
-        // Create a *new* object with changes
-        return { ...artwork, seen: nextSeen };
-      } else {
-        // No changes
-        return artwork;
-      }
-    }));
+import { useState } from "react";
+import React from "react";
+export default function FeedbackForm(){
+  const [text, setText]= useState('')
+  const [isSending, setIsSending]=useState(false)
+  const [isSent, setIsSent]= useState(false)
+   
+  async function handleSubmit(e){
+    e.preventDefault()
+    setIsSending(true)
+    await SendMessage(text)
+    setIsSending(false)
+    setIsSent(true)
   }
-
-  function handleToggleYourList(artworkId, nextSeen) {
-    setYourList(yourList.map(artwork => {
-      if (artwork.id === artworkId) {
-        // Create a *new* object with changes
-        return { ...artwork, seen: nextSeen };
-      } else {
-        // No changes
-        return artwork;
-      }
-    }));
+  if (isSent){
+    return <h1>Thanks for feedback!</h1>
   }
-
-  return (
-    <>
-      <h1>Art Bucket List</h1>
-      <h2>My list of art to see:</h2>
-      <ItemList
-        artworks={myList}
-        onToggle={handleToggleMyList} />
-      <h2>Your list of art to see:</h2>
-      <ItemList
-        artworks={yourList}
-        onToggle={handleToggleYourList} />
-    </>
-  );
+  return(
+    <form onSubmit={handleSubmit}>
+      <p>How was your stay at The Prancing Pony?</p>
+      <textarea 
+      disabled={isSending}
+      value={text}
+      onChange={e=>setText(e.target.value)}
+      />
+      <br/>
+      <button
+      disabled={isSending}
+      type="submit"
+      >
+        send
+      </button>
+      {isSending && <p>Sending...</p>}
+    </form>
+  )
 }
 
-function ItemList({ artworks, onToggle }) {
-  return (
-    <ul>
-      {artworks.map(artwork => (
-        <li key={artwork.id}>
-          <label>
-            <input
-              type="checkbox"
-              checked={artwork.seen}
-              onChange={e => {
-                onToggle(
-                  artwork.id,
-                  e.target.checked
-                );
-              }}
-            />
-            {artwork.title}
-          </label>
-        </li>
-      ))}
-    </ul>
-  );
+function SendMessage(text){
+  return new Promise(
+    resolve=>{
+        setTimeout(resolve,2000)
+    }
+  )
 }
