75d1c71ac67434ec3ee8ccfda5eafc14c49b5616
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-07 15:56:29 +0100

    Évitez les états redondants

--- src/App.js
+++ src/App.js
@@ -1,44 +1,36 @@
-import { useState } from "react";
-import React from "react";
-export default function FeedbackForm(){
-  const [text, setText]= useState('')
-  const [isSending, setIsSending]=useState(false)
-  const [isSent, setIsSent]= useState(false)
-   
-  async function handleSubmit(e){
-    e.preventDefault()
-    setIsSending(true)
-    await SendMessage(text)
-    setIsSending(false)
-    setIsSent(true)
+import { useState } from "react"
+
+export default function Form(){
+  const [firstname, setFirstname]=useState('')
+  const[lastname, setLastname]=useState('')
+  //const [fullname, setFullname]=useState('')
+  const handleFirstnameChange=(e)=>{
+    setFirstname(e.target.value)
+    //setFullname(e.target.value+ '' +lastname)
   }
-  if (isSent){
-    return <h1>Thanks for feedback!</h1>
+  const handleLatsnameChange=(e)=>{
+    setLastname(e.target.value)
+    //setFullname(firstname+ '' +e.target.value)
   }
   return(
-    <form onSubmit={handleSubmit}>
-      <p>How was your stay at The Prancing Pony?</p>
-      <textarea 
-      disabled={isSending}
-      value={text}
-      onChange={e=>setText(e.target.value)}
-      />
-      <br/>
-      <button
-      disabled={isSending}
-      type="submit"
-      >
-        send
-      </button>
-      {isSending && <p>Sending...</p>}
-    </form>
-  )
-}
-
-function SendMessage(text){
-  return new Promise(
-    resolve=>{
-        setTimeout(resolve,2000)
-    }
+   <>
+   <h1>Let’s check you in</h1>
+   <label>firstname:
+    <input
+      value={firstname}
+      onChange={handleFirstnameChange}
+ ></input>
+    </label>
+    <br/>
+    <br/>
+    <br/>
+<label>lastname:
+  <input
+    value={lastname}
+    onChange={handleLatsnameChange}
+  ></input>
+</label>
+<p>Your ticket will be issued to:</p>
+   </>
   )
 }
