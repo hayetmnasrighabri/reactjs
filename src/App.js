f9ed954b5bfff052e2c24e97983164215760f43f
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-08 09:20:23 +0100

    Élever l’État

--- src/App.js
+++ src/App.js
@@ -1,37 +1,33 @@
 import { useState } from "react"
 
-export default function Form(){
-  const [firstname, setFirstname]=useState('')
-  const[lastname, setLastname]=useState('')
-  //const [fullname, setFullname]=useState('')
-  const fullname=firstname+''+lastname
-  const handleFirstnameChange=(e)=>{
-    setFirstname(e.target.value)
-    //setFullname(e.target.value+ '' +lastname)
-  }
-  const handleLatsnameChange=(e)=>{
-    setLastname(e.target.value)
-    //setFullname(firstname+ '' +e.target.value)
-  }
-  return(
-   <>
-   <h1>Let’s check you in</h1>
-   <label>firstname:{''}
-    <input
-      value={firstname}
-      onChange={handleFirstnameChange}
- ></input>
-    </label>
-    <br/>
-    <br/>
-    <br/>
-<label>lastname:{''}
-  <input
-    value={lastname}
-    onChange={handleLatsnameChange}
-  ></input>
-</label>
-<p>Your ticket will be issued to:{fullname}</p>
-   </>
-  )
+
+function Panel({title, children}){
+const [isActive, setIsActive]= useState(false)
+return(
+  <>
+  <section className="panel">
+    <h3>{title}</h3>
+    {isActive ? (<p>{children}</p>): (
+      <button onClick={()=>setIsActive(true)}>
+        show
+      </button>
+    )}
+  </section>
+  </>
+)
 }
+export default function Accordion(){
+  return(
+    <>
+    <h2>Almaty, Kazakhstan</h2>
+    <Panel title="About">
+    With a population of about 2 million, Almaty is Kazakhstan's largest city. From 1929 to 1997, it was its capital city.
+    </Panel>
+    <Panel title="Etymology">
+    The name comes from <span lang="kk-KZ">алма</span>,
+     the Kazakh word for "apple" and is often translated as "full of apples". In fact, the region surrounding Almaty is thought to be the ancestral home of the apple, and the wild <i lang="la">Malus sieversii</i> is considered 
+    a likely candidate for the ancestor of the modern domestic apple.
+    </Panel>
+    </>
+  )
+} 
