0395959ccb8e1aa858a6f10c5d6d482e28b726fd
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-09 16:33:59 +0100

    commit 2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
-import Section from "./Section"
-import Heading from "./Heading"
+import Heading from './Heading.js';
+import Section from './Section.js';
 
-export default function Page(){
-  return(
-    <section>
+export default function Page() {
+  return (
+    <Section>
       <Heading level={1}>Title</Heading>
-      <Heading level={2}>Heading</Heading>
-      <Heading level={3}>Sub-heading</Heading>
-      <Heading level={4}>Sub-sub-heading</Heading>
-      <Heading level={5}>Sub-sub-sub-heading</Heading>
-      <Heading level={6}>Sub-sub-sub-sub-heading</Heading>
-    </section> 
-  )
+      <Section>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Heading level={2}>Heading</Heading>
+        <Section>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Heading level={3}>Sub-heading</Heading>
+          <Section>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+            <Heading level={4}>Sub-sub-heading</Heading>
+          </Section>
+        </Section>
+      </Section>
+    </Section>
+  );
 }
