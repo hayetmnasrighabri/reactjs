7ad0dc3404b02ad5e470bf2109316a410aac937a
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-09 19:54:41 +0100

    construire un chronomètre

--- src/App.js
+++ src/App.js
@@ -1,16 +1,37 @@
-import { useRef } from "react";
+import { useState } from 'react';
+import { useRef } from 'react';
 
-export default function Counter(){
-  let ref = useRef(0)
-  const handleClick=()=>{
-    ref.current= ref.current+1
-    alert('you clicked' +ref.current+'time!')
+export default function Stopwatch() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const intervalRef= useRef (null)
+  function handleStart() {
+    setStartTime(Date.now());
+    setNow(Date.now());
+    
+    clearInterval(intervalRef.current)
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
   }
-  return(
+  function handleStop(){
+    clearInterval(intervalRef.current)
+  }
+
+  let secondsPassed = 0;
+  if (startTime != null && now != null) {
+    secondsPassed = (now - startTime) / 1000;
+  }
+
+  return (
     <>
-    <button onClick={handleClick}>
-      click me
-    </button>
+      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
+      <button onClick={handleStart}>
+        Start
+      </button>
+       <button onClick={handleStop}>
+        stop
+       </button>
     </>
-  )
+  );
 }
