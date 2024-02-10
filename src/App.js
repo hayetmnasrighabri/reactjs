2fb914f506b5f2d86c43ee035304fada09d46c73
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-10 11:12:06 +0100

    Faire défiler jusqu'à un élément

--- src/App.js
+++ src/App.js
@@ -1,15 +1,66 @@
 import { useRef } from "react";
-export default function Form(){
-  const inputRef= useRef(null)
-  const handleClick=()=>{
-    inputRef.current.focus()
+ export default function CatFriends(){
+  const firstCatRef=useRef(null)
+  const secondCatRef=useRef(null)
+  const thirdCatRef=useRef(null)
+
+  const handleScrollFirstCat=()=>{
+    firstCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    })
   }
-  return (
+    const handleScrollToSecondCat=()=>{
+      secondCatRef.current.scrollIntoView({
+        behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+      })
+    }
+
+   const handleScrollToThirdCat=()=> {
+    thirdCatRef.current.scrollIntoView({
+      behavior: 'smooth',
+      block: 'nearest',
+      inline: 'center'
+    })
+  }
+  return(
     <>
-    <input ref={inputRef}/>
-    <button onClick={handleClick}>
-    Focus the input
-    </button>
+    <nav>
+      <button onClick={handleScrollFirstCat}>
+          tom
+      </button>
+      <button onClick={handleScrollToSecondCat}>
+      maru
+      </button>
+      <button onClick={handleScrollToThirdCat}>
+      Jellylorum
+      </button>
+    </nav>
+    <div>
+      <ul>
+        <li>
+          <img 
+          src="https://placekitten.com/g/200/200"
+          alt="tom"
+          ref={firstCatRef}/>
+        </li>
+        <li>
+          <img 
+          src="https://placekitten.com/g/300/200"
+          alt="maru"
+          ref={secondCatRef}/>
+        </li>
+        <li>
+          <img
+          src="https://placekitten.com/g/250/200"
+          alt="Jellylorum"
+          ref={thirdCatRef}/>
+        </li>
+      </ul>
+    </div>
     </>
   )
-}
+ }
