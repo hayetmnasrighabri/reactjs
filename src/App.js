55fe37578b586e8d51c35e74488e8180a7cfc569
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-10 10:48:09 +0100

    focaliser une saisie de texte

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
-import { useState } from 'react';
-import { useRef } from 'react';
-
-export default function Stopwatch() {
-  const [startTime, setStartTime] = useState(null);
-  const [now, setNow] = useState(null);
-  const intervalRef= useRef (null)
-  function handleStart() {
-    setStartTime(Date.now());
-    setNow(Date.now());
-    
-    clearInterval(intervalRef.current)
-    intervalRef.current = setInterval(() => {
-      setNow(Date.now());
-    }, 10);
+import { useRef } from "react";
+export default function Form(){
+  const inputRef= useRef(null)
+  const handleClick=()=>{
+    inputRef.current.focus()
   }
-  function handleStop(){
-    clearInterval(intervalRef.current)
-  }
-
-  let secondsPassed = 0;
-  if (startTime != null && now != null) {
-    secondsPassed = (now - startTime) / 1000;
-  }
-
   return (
     <>
-      <h1>Time passed: {secondsPassed.toFixed(3)}</h1>
-      <button onClick={handleStart}>
-        Start
-      </button>
-       <button onClick={handleStop}>
-        stop
-       </button>
+    <input ref={inputRef}/>
+    <button onClick={handleClick}>
+    Focus the input
+    </button>
     </>
-  );
+  )
 }
