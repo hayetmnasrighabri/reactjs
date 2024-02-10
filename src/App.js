c98ba840ec54cea35153e3e4ee9fabd5c513c849
hayetmnasri <hayetmnasri@hotmail.fr>
2024-02-10 16:01:40 +0100

    déclarer un effet

--- src/App.js
+++ src/App.js
@@ -1,22 +1,30 @@
-import { forwardRef, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 
-const MyInput = forwardRef((props, ref) => {
-  return <input {...props} ref={ref} />;
-});
+function VideoPlayer({ src, isPlaying }) {
+  const ref = useRef(null);
 
-export default function Form() {
-  const inputRef = useRef(null);
+  useEffect(() => {
+    if (isPlaying) {
+      ref.current.play();
+    } else {
+      ref.current.pause();
+    }
+  });
 
-  function handleClick() {
-    inputRef.current.focus();
-  }
+  return <video ref={ref} src={src} loop playsInline />;
+}
 
+export default function App() {
+  const [isPlaying, setIsPlaying] = useState(false);
   return (
     <>
-      <MyInput ref={inputRef} />
-      <button onClick={handleClick}>
-        Focus the input
+      <button onClick={() => setIsPlaying(!isPlaying)}>
+        {isPlaying ? 'Pause' : 'Play'}
       </button>
+      <VideoPlayer
+        isPlaying={isPlaying}
+        src="https://interactive-examples.mdn.mozilla.net/media/cc0-videos/flower.mp4"
+      />
     </>
   );
 }
